1d870a826bfa3ae250f88218df9b4ca6975957b5
Joel.Rosario <joel@xnsio.com>
2020-04-29 17:30:32 +0530

    Now using stub functionality instead of contract mock

--- src/test/kotlin/controllers/APITestsViaJUnit.kt
+++ src/test/kotlin/controllers/APITestsViaJUnit.kt
@@ -10,44 +10,23 @@ import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.http.*
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.postForEntity
-import run.qontract.core.HttpRequest
-import run.qontract.core.HttpResponse
 import run.qontract.core.getContractFileName
 import run.qontract.core.versioning.contractNameToRelativePath
-import run.qontract.mock.ContractMock
-import run.qontract.mock.MockScenario
+import run.qontract.fake.createStubFromContractAndData
 import java.io.File
 import java.net.URI
 import java.nio.file.Paths
 import kotlin.test.assertEquals
 
 class APITestsViaJUnit {
-    private val petStoreContract: String = getContractText("run.qontract.examples.petstore", 1, 1)
-
     @Test
     fun `search for available dogs`() {
-        ContractMock(petStoreContract, 9000).use { mock ->
-            mock.start()
-
-            val mockRequest = HttpRequest()
-                    .updateMethod("GET")
-                    .updatePath("/pets")
-                    .updateQueryParam("type", "dog")
-                    .updateQueryParam("status", "available")
-                    .updateQueryParam("name", "")
-            val mockResponse = HttpResponse(200, """[{"name": "Archie", "type": "dog", "status": "available", "id": 10}, {"name": "Reggie", "type": "dog", "status": "available", "id": 20}]""")
-            val scenario = MockScenario(mockRequest, mockResponse)
-            mock.createMockScenario(scenario)
-
             val response = query("http://localhost:8080/findFirstAvailablePet?type=dog")
             assertEquals(HttpStatus.OK, response.statusCode)
 
             val petInfo = JSONObject(response.body)
 
             assertValuesExpected(petInfo, 10, "ARCHIE")
-        }
-
-        Thread.sleep(5000)
     }
 
     private fun assertValuesExpected(pet: JSONObject, id: Int, name: String) {
@@ -62,14 +41,7 @@ class APITestsViaJUnit {
 
     @Test
     fun `create pet`() {
-        ContractMock(petStoreContract, 9000).use { mock ->
-            mock.start()
             val requestBody = """{"name": "Archie", "type": "dog", "status": "available"}"""
-
-            val mockRequest = HttpRequest().updateMethod("PUT").updatePath("/pets").updateBody(requestBody)
-            val mockResponse = HttpResponse(200, "12")
-            val scenario = MockScenario(mockRequest, mockResponse)
-            mock.createMockScenario(scenario)
 
             val apiClient = RestTemplate()
             val url = URI.create("http://localhost:8080/create-pet")
@@ -80,7 +52,6 @@ class APITestsViaJUnit {
             val jsonResponse = JSONObject(responseEntity.body)
             assertEquals("success", jsonResponse.getString("status"))
             assertEquals(12, jsonResponse.getInt("id"))
-        }
     }
 
     private fun jsonRequest(url: URI, requestBody: String): RequestEntity<String> {
@@ -90,7 +61,9 @@ class APITestsViaJUnit {
     }
 
     companion object {
-        var service: ConfigurableApplicationContext? = null
+        private var service: ConfigurableApplicationContext? = null
+        private val contractGherkin: String = getContractText("run.qontract.examples.petstore", 1, 1)
+        private val stub = createStubFromContractAndData(contractGherkin, "./src/test/resources/petstore_data")
 
         @BeforeAll
         @JvmStatic
@@ -102,6 +75,7 @@ class APITestsViaJUnit {
         @JvmStatic
         fun tearDown() {
             service?.stop()
+            stub.close()
         }
     }
 }
